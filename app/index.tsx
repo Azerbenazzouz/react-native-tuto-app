35563b61c0640e6d0c1faca2431d5439700bfa03
Azer Ben Azzouz <benazzouzazer@gmail.com>
2024-07-04 13:52:50 +0100

    assets and const

--- app/index.tsx
+++ app/index.tsx
@@ -1,22 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Text, View } from 'react-native'
 import React from 'react'
 import { Link } from 'expo-router'
 
 const index = () => {
   return (
-    <View style={styles.container}>
-      <Text>index</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl">index</Text>
       <Link href="profile">profile</Link>
     </View>
   )
 }
 
 export default index
-
-const styles = StyleSheet.create({
-    container : {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
-})
