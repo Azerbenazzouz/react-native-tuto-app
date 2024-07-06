1e4173dfce0e11829a9c56f547d1293431d20331
Azer Ben Azzouz <benazzouzazer@gmail.com>
2024-07-06 23:15:33 +0100

    auth context

---
+++ context/GlobalProvider.js
@@ -0,0 +1,45 @@
+import { createContext, useContext, useState, useEffect } from "react";
+import { getCurrentUser } from "../lib/appWrite";
+
+const GlobalContext = createContext();
+export const useGlobalContext = () => useContext(GlobalContext);
+
+const GlobalProvider = ({ children }) => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [user, setUser] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        getCurrentUser
+        .then((res)=>{
+            if(res){
+                setUser(res);
+                setIsLoggedIn(true);
+            }else{
+                setIsLoading(false);
+                setUser(null);
+            }
+        })
+        .catch((error)=>{
+            console.log(error);
+        })
+        .finally(()=>{
+            setIsLoading(false);
+        })
+    }, [])
+    
+    return (
+        <GlobalContext.Provider value={{
+            isLoggedIn,
+            setIsLoggedIn,
+            user,
+            setUser,
+            isLoading,
+            setIsLoading
+        }}>
+            {children}
+        </GlobalContext.Provider>
+    )
+};
+
+export default GlobalProvider;
