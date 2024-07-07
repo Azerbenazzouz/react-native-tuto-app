140d1e8443a5d4cac866cd4169aeb070bb111cf2
Azer Ben Azzouz <benazzouzazer@gmail.com>
2024-07-07 10:12:20 +0100

    home

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,11 +1,24 @@
-import { View, Text } from 'react-native'
+import { View, Text, FlatList } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const Home = () => {
   return (
-    <View>
+    <SafeAreaView>
+      <FlatList
+        data={[{id: 'a'}, {id: 'b'}]}
+        keyExtractor={(item) => item.id}
+        renderItem={({item}) => <Text className="text-3xl">{item.id}</Text>}
+        ListHeaderComponent={() => (
+          <View className="my-6 px-4 space-y-6">
+            <View className="justify-between items-start flex-row mb-6">
+              <Text className="text-3xl">Home</Text>
+            </View>
+          </View>
+        )}
+      />
       <Text>Home</Text>
-    </View>
+    </SafeAreaView>
   )
 }
 
